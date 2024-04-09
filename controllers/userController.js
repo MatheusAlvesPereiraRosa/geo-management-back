3f0bda428a16fbbcb6eef696bc233cc4a65c6844
MatheusAlves <matheus.alves@empirica.digital>
2024-04-09 10:59:58 -0300

    Melhorando criação de usuário

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,21 @@ const User = require('../models/user');
 const UserController = {
     createUser: async (req, res) => {
         try {
-            const user = await User.create(req.body);
-            res.status(200).json(user);
+            console.log(req.body)
+            const { name, email, phoneNumber, coordinates } = req.body;
+
+            const user = await User.create({
+                name,
+                email,
+                phoneNumber,
+                coordinatesX: coordinates.x,
+                coordinatesY: coordinates.y,
+            })
+
+            res.json(user);
         } catch (error) {
-            console.error(error);
-            res.status(500).send('Server Error');
+            console.error('Error creating user:', error);
+            throw error;
         }
     },
 
@@ -45,7 +55,7 @@ const UserController = {
             const user = await User.findAll({
                 where: whereClause,
             });
-            
+
             res.json(user)
         } catch (error) {
             console.error(error)
